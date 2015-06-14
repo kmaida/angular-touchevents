5d1cf2a7ddff415552ab9fd86b2ab6e5f0f2bfae
Kim Maida <git@kim-maida.com>
2015-06-14 13:44:00 -0400

    * Add touchstart, touchend, touchmove (with $event). * Add demo.

--- demo/ng-app.js
+++ demo/ng-app.js
@@ -1,10 +1,32 @@
 (function() {
 	angular
-		.module('myApp', [])
+		.module('myApp', ['ng-touchevents'])
 		.controller('MainCtrl', [
 			'$scope',
 			function($scope) {
 
+				/**
+				 * Button touch start
+				 */
+				$scope.touchstartBtn = function() {
+					alert('Button touch start');
+				};
+
+				/**
+				 * Button touch end
+				 */
+				$scope.touchendBtn = function() {
+					alert('Button touch end');
+				};
+
+				/**
+				 * Button touch move
+				 *
+				 * @param $event {object} touch event
+				 */
+				$scope.touchmoveBtn = function($event) {
+					console.log('move event', $event);
+				};
 			}
 		]);
 })();
--- ng-touchevents.js
+++ ng-touchevents.js
@@ -4,7 +4,8 @@
 	angular
 		.module('ng-touchevents', [])
 		.directive('ngTouchstart', ngTouchstart)
-		.directive('ngTouchend', ngTouchend);
+		.directive('ngTouchend', ngTouchend)
+		.directive('ngTouchmove', ngTouchmove);
 
 	//---- Touchstart
 
@@ -12,35 +13,27 @@
 
 	function ngTouchstart() {
 
-		ngTouchstartLink.$inject = ['$scope', '$elm', '$attrs'];
+		ngTouchstartLink.$inject = ['$scope', '$elm'];
 
 		/**
 		 * ngTouchstart directive link function
 		 *
 		 * @param $scope {object}
 		 * @param $elm {object} DOM element
-		 * @param $attrs {object} HTML attributes
 		 */
-		function ngTouchstartLink($scope, $elm, $attrs) {
-			$elm.bind('touchstart', onTouchStart);
-
-			function onTouchStart() {
-				var method = $attrs.ngTouchstart;
-
-				console.log('touchstart', method);
-
-				$scope.$apply(function() {
-					$scope.$eval(method);
-				});
-			}
+		function ngTouchstartLink($scope, $elm) {
+			$elm.bind('touchstart', $scope.ngTouchstart);
 
 			$scope.$on('$destroy', function() {
-				$elm.unbind('touchstart', onTouchStart);
+				$elm.unbind('touchstart', $scope.ngTouchstart);
 			});
 		}
 
 		return {
 			restrict: 'A',
+			scope: {
+				ngTouchstart: '&'
+			},
 			link: ngTouchstartLink
 		};
 	}
@@ -51,36 +44,63 @@
 
 	function ngTouchend() {
 
-		ngTouchendLink.$inject = ['$scope', '$elm', '$attrs'];
+		ngTouchendLink.$inject = ['$scope', '$elm'];
 
 		/**
 		 * ngTouchend directive link function
 		 *
 		 * @param $scope {object}
 		 * @param $elm {object} DOM element
-		 * @param $attrs {object} HTML attributes
 		 */
-		function ngTouchendLink($scope, $elm, $attrs) {
-			$elm.bind('touchend', onTouchEnd);
-
-			function onTouchEnd() {
-				var method = $attrs.ngTouchend;
-
-				console.log('touchend', method);
-
-				$scope.$apply(function() {
-					$scope.$eval(method);
-				});
-			}
+		function ngTouchendLink($scope, $elm) {
+			$elm.bind('touchend', $scope.ngTouchend);
 
 			$scope.$on('$destroy', function() {
-				$elm.unbind('touchend', onTouchEnd);
+				$elm.unbind('touchend', $scope.ngTouchend);
 			});
 		}
 
 		return {
 			restrict: 'A',
+			scope: {
+				ngTouchend: '&'
+			},
 			link: ngTouchendLink
+		};
+	}
+
+	//---- Touchmove
+
+	ngTouchmove.$inject = [];
+
+	function ngTouchmove() {
+
+		ngTouchmoveLink.$inject = ['$scope', '$elm'];
+
+		/**
+		 * ngTouchmove directive link function
+		 *
+		 * @param $scope {object}
+		 * @param $elm {object} DOM element
+		 */
+		function ngTouchmoveLink($scope, $elm) {
+			$elm.bind('touchmove', onTouchMove);
+
+			function onTouchMove($event) {
+				$scope.ngTouchmove({$event: $event});
+			}
+
+			$scope.$on('$destroy', function() {
+				$elm.unbind('touchstart', onTouchMove);
+			});
+		}
+
+		return {
+			restrict: 'A',
+			scope: {
+				ngTouchmove: '&'
+			},
+			link: ngTouchmoveLink
 		};
 	}
 })();
